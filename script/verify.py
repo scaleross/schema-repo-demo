08c4fa02c47c61cf13f54e1f368c371837e09e26
tianhao_wang <tianhaow1989@gmail.com>
2021-07-07 18:56:40 -0700

    add action

--- script/verify.py
+++ script/verify.py
@@ -7,4 +7,8 @@ onlydirs = [f for f in listdir(mypath) if isdir(join(mypath, f))]
 for dir in onlydirs:
     if dir.startswith('.') or dir == 'script':
         continue
-    print(dir)
+    print(f"{dir}:")
+    current_path = './' + str(dir) + '/'
+    onlyfiles = [f for f in listdir(current_path) if isfile(join(current_path, f))]
+    for file in onlyfiles:
+        print(file)
