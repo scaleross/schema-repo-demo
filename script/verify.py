63c5b843ecdcfd0e86ba388af9471e7e328215d4
tianhao_wang <tianhaow1989@gmail.com>
2021-07-07 19:14:42 -0700

    add action

--- script/verify.py
+++ script/verify.py
@@ -10,6 +10,42 @@ for dir in onlydirs:
     print(f"{dir}:")
     current_path = './' + str(dir) + '/'
     onlyfiles = [f for f in listdir(current_path) if isfile(join(current_path, f))]
+    versions = {}
     for file in onlyfiles:
         print(file)
+        next_version = is_valid_migration_name(name)
+        if next_version in versions:
+        	print(f'find duplicate version in {dir}')
+        	exit(1)
+
+    	print(next_version)
+        versions.add(next_version)
+
+
     exit(1)
+
+def is_valid_migration_name(name):
+	if len(name) > 64:
+		print(f'filename too long: {name}')
+		exit(1)
+
+	if not name.endswith('.sql'):
+		print(f'{name} not with sql extension: {name}')
+		exit(1)
+
+	parts = name.split('_')
+	if len(parts < 2):
+		print(f'{name} not contains _')
+		exit(1)
+
+	version = parts[0]
+	if len(version) != 4:
+		print(f'version {version} not in good length.')
+		exit(1)
+
+	if not version.isnumeric():
+		print(f'version {version} not numeric')
+
+	return Integer.parseInt(version);  
+
+    
