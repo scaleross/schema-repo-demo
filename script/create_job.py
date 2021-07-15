93fd92876e9184b6f2509d4f8714c6ca9757ac1b
tianhao_wang <tianhaow1989@gmail.com>
2021-07-15 16:28:20 -0700

    checkin file

--- script/create_job.py
+++ script/create_job.py
@@ -6,9 +6,16 @@ hash_val = sys.argv[HASH_INDEX]
 
 print(f"handling {hash_val}")
 
+keyspace = ""
 with open("changed_files.meta", "r") as changed_files:
     lines = [line.rstrip() for line in changed_files]
     for line in lines:
-        print(line)
+        splits = line.split('/')
+        if splits == 2:
+        	keyspace = splits[0]
+        	break
+
+
+print(keyspace)
 
 
