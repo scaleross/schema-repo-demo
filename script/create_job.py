294ef66d365011341845e800bd5dfa0a95a80711
tianhao_wang <tianhaow1989@gmail.com>
2021-07-15 16:19:27 -0700

    checkin file

--- script/create_job.py
+++ script/create_job.py
@@ -7,8 +7,8 @@ hash_val = sys.argv[HASH_INDEX]
 print(f"handling {hash_value}")
 
 with open("changed_files.meta", "r") as changed_files:
-	lines = [line.rstrip() for line in changed_files]
-    	for line in lines: 
-    		print(line)
+    lines = [line.rstrip() for line in changed_files]
+        for line in lines: 
+            print(line)
 
 
